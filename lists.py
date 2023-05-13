4bf517faae24697ee107e8bf761f4d6348aec1c0
leilaashur <leilahashur@gmail.com>
2023-05-13 10:52:15 +0300

    first commit

--- lists.py
+++ lists.py
@@ -96,6 +96,41 @@ def apply_function(func, *args):
 # keyword arguments. The function should return a new list where each 
 # integer in the original list has been multiplied by the value of
 # the corresponding keyword argument.
+def multiply_list(list, **kwargs):
+    empty = []
+    for num in list:
+        for key, value in kwargs.items():
+            if int(key) == len(empty):
+                empty.append(num * value)
+            else:
+                empty[len(empty)-1] *= value
+    return empty
+
+# Write a function that takes a list of integers and an unknown number of 
+# additional integers as arguments. The function should return the index
+# of the first occurrence of the sum of the list and the additional integers
+def index_of_occurence(list,*args):
+    
+    total = sum(list) + sum(args)
+    for i, num in enumerate(list):
+        if num == total:
+            return i
+    return -1  
+lst = [1, 2, 3, 4]
+index = index_of_occurence(lst, 5, 6)
+print(index)
+
+# Write a function that takes an unknown number of keyword arguments, each with 
+# a string value. The function should concatenate all the strings and return 
+# the resulting string.
+def concatenate_strings(**kwargs):
+    concatenated_string = ""
+    for key, value in kwargs.items():
+        concatenated_string += value
+    return concatenated_string
+concatenate_strings(first_name="Leila", last_name="Ashur")
+
+    
 
 
 
